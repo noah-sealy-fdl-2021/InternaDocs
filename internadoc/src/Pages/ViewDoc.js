fdd402aabbd7aae8ae1c195763b54244abbf62e3
nsealy <noah.sealy8@gmail.com>
2021-03-06 12:21:24 -0400

    added pdf viewer

--- internadoc/src/Pages/ViewDoc.js
+++ internadoc/src/Pages/ViewDoc.js
@@ -1,13 +1,31 @@
 import MainNavBar from '../Components/MainNavBar';
 import SearchBar from '../Components/SearchBar';
-import { useState, useEffect } from 'react';
+import WebViewer from '@pdftron/webviewer';
+import { useState, useEffect, useRef } from 'react';
+import pdf from '../pdf/p1.pdf'
+import '../App.css';
 
 const ViewDoc = ({ match }) => {
+    
+    const viewerDiv = useRef(null);
 
-    const [doc, setDoc] = useState([]);
+    const [doc, setDoc] = useState({
+        name: '',
+        author: '',
+        accessLevel: '',
+        docType: '',
+        link: '',
+    });
 
-    //similar to a mountComponent, but for functions (not classes), hooks!
+    // similar to a mountComponent, but for functions (not classes), hooks!
+    // gets called on page load, otherwise specify when it gets called in the [] at the end
     useEffect(() => {
+
+        WebViewer({path: 'lib', initialDoc: pdf}, viewerDiv.current)
+            .then(instance => {
+                // call pdf apis here
+            });
+
         fetchItem();
     }, []);
 
@@ -26,11 +44,13 @@ const ViewDoc = ({ match }) => {
             <SearchBar />
             <br />
             <br />
-            <h1>{doc.name}</h1>
-            <h2>{doc.author}</h2>
+            <h3>{doc.name} . {doc.author}</h3>
+            {/* <h2>{doc.author}</h2>
             <h2>{doc.accessLevel}</h2>
             <h2>{doc.docType}</h2>
-            <h2>{doc.link}</h2>
+            <h2>{doc.link}</h2> */}
+
+            <div className='webViewer' ref={viewerDiv}></div>
 
         </div>
     )
